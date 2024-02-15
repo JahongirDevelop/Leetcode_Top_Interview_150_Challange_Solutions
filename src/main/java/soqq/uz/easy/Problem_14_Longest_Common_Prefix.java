7a315b5a6eb9ccf264a3db8b0323f48f0656dde5
Jahongir <sjahongir22@gmail.com>
2024-02-15 21:30:20 +0500

    Problem_14_Longest_Common_Prefix

---
+++ src/main/java/soqq/uz/easy/Problem_14_Longest_Common_Prefix.java
@@ -0,0 +1,30 @@
+package soqq.uz.easy;
+
+import java.util.Arrays;
+
+public class Problem_14_Longest_Common_Prefix {
+    /*
+      Path :
+      https://leetcode.com/problems/longest-common-prefix
+      */
+
+    public static void main(String[] args) {
+        String[] strings = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strings));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+}
