ac1438f6315fb368c1ddcf0d6394e14b9d0b02c7
JahongirDevelop <sjahongir221@gmail.com>
2024-02-23 20:34:11 +0500

    Problem_997_Find_The_Town_Judge

--- src/main/java/soqq/uz/easy/Problem_997_Find_The_Town_Judge.java
+++ src/main/java/soqq/uz/easy/Problem_997_Find_The_Town_Judge.java
@@ -4,4 +4,18 @@ public class Problem_997_Find_The_Town_Judge {
     public static void main(String[] args) {
 
     }
+
+    public static int findJudge(int n, int[][] trust) {
+        int[] inout = new int[n+1];
+        for(int[] i:trust){
+            inout[i[0]]-=1;
+            inout[i[1]]+=1;
+        }
+        for(int i=1;i<=n;i++){
+            if(inout[i] == n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
