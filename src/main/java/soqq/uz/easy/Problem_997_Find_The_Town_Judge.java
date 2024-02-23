5ee6bc3853848d1784777b22811f6925bd3a2259
JahongirDevelop <sjahongir221@gmail.com>
2024-02-23 20:37:42 +0500

    Problem_997_Find_The_Town_Judge

--- src/main/java/soqq/uz/easy/Problem_997_Find_The_Town_Judge.java
+++ src/main/java/soqq/uz/easy/Problem_997_Find_The_Town_Judge.java
@@ -2,7 +2,9 @@ package soqq.uz.easy;
 
 public class Problem_997_Find_The_Town_Judge {
     public static void main(String[] args) {
-
+    int n = 3;
+    int[][] trust = {{1,3},{2,3},{3,1}};
+        System.out.println(findJudge(n, trust));
     }
 
     public static int findJudge(int n, int[][] trust) {
